d4de20a685b90fec36c0338a46af7319849871d6
Jouni Helske <jouni.helske@jyu.fi>
2016-11-28 14:29:25 +0000

    examples

--- R/update.R
+++ R/update.R
@@ -10,7 +10,7 @@
 #' @return Updated L.
 #' @export
 chol_update <- function(L, u) {
-  cholupdateR(L, u)
+  chol_updateR(L, u)
 }
 #' Downdate Cholesky Decomposition
 #'
@@ -25,7 +25,7 @@ chol_update <- function(L, u) {
 #' @return Updated L.
 #' @export
 chol_downdate <- function(L, u) {
-  L <- choldowndateR(L, u)
+  L <- chol_downdateR(L, u)
   if(any(!is.finite(L)) || any(diag(L) < 0)) {
     stop("Resulting matrix is not positive definite.")
   }
@@ -47,7 +47,42 @@ chol_downdate <- function(L, u) {
 #' @param gamma Scaling parameter. Default is 2/3.
 #' @return Updated S.
 #' @export
+#' @examples
+#'
+#' # sample from standard normal distribution
+#'
+#' adapt_mcmc <- function(n = 10000, sigma) {
+#'   x <- numeric(n)
+#'   loglik_old <- dnorm(x[1])
+#'   for (i in 2:n) {
+#'     u <- rnorm(1, sd = sigma)
+#'     prop <- x[i] + u
+#'     loglik <- dnorm(prop)
+#'     accept_prob <- min(1, loglik/loglik_old)
+#'     if (runif(1) < accept_prob) {
+#'       x[i] <- prop
+#'       loglik_old <- loglik
+#'     } else {
+#'       x[i] <- x[i - 1]
+#'     }
+#'     if (i < n/2) {
+#'       sigma <- update_S(sigma, u, accept_prob, i, 0.44)
+#'     }
+#'   }
+#'   list(x = x[(n/2):n], sigma = sigma)
+#' }
+#'
+#' out <- adapt_mcmc(1e4, 2)
+#' out$sigma
+#' hist(out$x)
+#' # acceptance rate:
+#' 1 / mean(rle(out$x)$lengths)
 update_S <- function(S, u, current, n, target = 0.234, gamma = 2/3) {
+
+  if(!is.matrix(S) && length(S) == 1) {
+    S <- as.matrix(S)
+  }
+
   if(any(S[upper.tri(S)] != 0)) {
     stop("S must be lower triangular matrix.")
   }
